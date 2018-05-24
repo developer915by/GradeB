8a10599988230d71c65e1ba6d21cbd862cedd3e1
developer915by <30619913+developer915by@users.noreply.github.com>
2018-05-24 21:40:42 +0300

    Progect
    grade student

---
+++ Grade6.9/Grade6.9/GBTest.cs
@@ -0,0 +1,12 @@
+﻿public  class GBTest
+{
+    public static void Main (string[] args)
+    {
+        GradeB myGradeB = new GradeB("Light to C# Programming");
+
+        myGradeB.DisplayMessage();
+        myGradeB.InputGrades();
+        myGradeB.DisplayGradeReport();
+    }
+    
+}
